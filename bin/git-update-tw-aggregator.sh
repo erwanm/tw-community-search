b14d6759e5bbe64c11f8042fbb21accebc96d0fb
Erwan Moreau <moreaue@cs.tcd.ie>
2015-08-28 00:16:58 +0200

    new version git script

--- bin/git-update-tw-aggregator.sh
+++ bin/git-update-tw-aggregator.sh
@@ -1,15 +1,46 @@
 #!/bin/bash
 
-# requires a properly configured git repo bith local and remote (e.g. github)
-# the basic repo with the wiki must already exist in the repo
+# requires a properly configured git repo both local and remote (e.g. github)
+# the skeleton wiki must already exist in the repo
 
 # must be run from the project dir
 
-
-# just in case, but things will go wrong if merging is needed
+echo "step 1: set git repo right"
+git checkout automatic
+if [ $? -ne 0 ]; then
+    echo "Error, impossible to checkout 'automatic' branch" 1>&2
+    exit 1
+fi
 git pull
-# run the aggregator
-tw-community-search.sh
-git commit -m "automatic update" tw-community-search.html
+
+echo "step 2: backup previous node.js content"
+rm -rf tw-community-search.bak
+mv tw-community-search tw-community-search.bak
+
+echo "step 3: run the aggregator"
+workDir=$(mktemp -d)
+tw-community-search.sh -d "$workDir"
+
+echo "step 4: extract the output wiki (node.js and html)"
+mv "$workDir"/output-wiki tw-community-search
+rm -rf "$workDir"
+
+echo "step 5: send the single html output somewhere else"
+tmpFile=$(mktemp)
+mv tw-community-search.html "$tmpFile"
+
+echo "step 6: committing and pushing changes for node.js version"
+git add -A tw-community-search/tiddlers/
+git commit -m "automatic update" 
+git push
+
+echo "step 7: switching to gh-pages"
+git checkout gh-pages
+rm -f index.html
+mv "$tmpFile" index.html
+
+echo "step 8: committing and pushing changes for single html"
+git add index.html
+git commit -m "automatic update"
 git push
 
